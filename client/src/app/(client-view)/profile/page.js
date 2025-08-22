5ee387c49e811f947194caa4c418f4c09e8dee28
uttom-kumar <uttom00781@gmail.com>
2025-08-23 00:30:47 +0600

    add database

--- client/src/app/(client-view)/profile/page.js
+++ client/src/app/(client-view)/profile/page.js
@@ -3,10 +3,12 @@ import React, {useEffect} from 'react';
 import ProfilePage from "@/component/client/profile/Profile-page";
 import {ReadCartRequest} from "@/component/Request-Api/CartRequest";
 import {ReadWishRequest} from "@/component/Request-Api/WishRequest";
+import {UserReadProfileRequest} from "@/component/Request-Api/UserProfileRequest";
 
 const Page = () => {
     useEffect(() => {
         (async () => {
+            await UserReadProfileRequest();
             await ReadCartRequest()
             await ReadWishRequest()
         })()
